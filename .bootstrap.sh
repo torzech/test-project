fe1f3b7a19f0e0af777373ca9dfbbdc394d3e129
Tomasz Orzechowski <t.orzechowski@webinnovations.pl>
2014-10-02 08:23:30 +0200

    final?

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 apt-get update
+apt-get install -y software-properties-common
+add-apt-repository ppa:ondrej/php5-5.6
+apt-get update
 # apt-get upgrade -y
-apt-get install -y apache2
+apt-get install -y apache2 php5
 rm -rf /var/www
 ln -fs /vagrant /var/www
 sed -i 's/\/var\/www\/html/\/var\/www/' /etc/apache2/sites-available/000-default.conf
---
+++ index.php
@@ -0,0 +1,2 @@
+<h1>It's alive!</h1>
+<?php phpinfo(); ?>
